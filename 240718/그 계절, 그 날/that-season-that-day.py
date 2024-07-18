2394b524a263a38fae5eef1a6603b9e4167c3a4b
simple-is-great <103080930+simple-is-great@users.noreply.github.com>
2024-07-18 20:59:16 +0900

    Title: 그 계절, 그 날, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240718/그 계절, 그 날/that-season-that-day.py
+++ 240718/그 계절, 그 날/that-season-that-day.py
@@ -14,13 +14,17 @@ def isLeapYear(year):
 def returnSeason(year, month, day):
     if (not isLeapYear(year) and month == 2 and day == 29): # 윤년이 아닌데, 29일 -> 오류
         return -1
+    # 예외 경우 더 있음, 30일인지, 31일인지 체크
+    smallMonth = [2, 4, 6, 9, 11]
+    if month in smallMonth and day == 31: # 작은 달인데 31일 있음
+        return -1
     else:
         if (month >= 3 and month <= 5):
             return "Spring"
         elif (month >= 6 and month <= 8):
             return "Summer"
         elif (month >= 9 and month <= 11):
-            return "Autumn"
+            return "Fall"
         elif (month == 12 or month == 1 or month == 2):
             return "Winter"
             
