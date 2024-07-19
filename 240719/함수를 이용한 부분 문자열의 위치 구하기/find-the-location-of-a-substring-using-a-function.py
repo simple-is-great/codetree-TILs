67d15eb6a0290a29b4970891c1f5d3f030ee3307
simple-is-great <103080930+simple-is-great@users.noreply.github.com>
2024-07-19 17:05:28 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 132ms, Memory: 73MB, Status: Passed - CodeTree

--- 240719/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 240719/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -2,6 +2,7 @@ input_string = input()
 target_string = input()
 
 def compareString(str_):
-    return input_string.find(target_string)
+    return input_string.find(target_string) 
+    # string.find(subString, [start], [end]): string에서 subString 찾아보고, 있으면 첫 번째 인덱스 반환, 없으면 -1 반환
     
 print(compareString(target_string))
