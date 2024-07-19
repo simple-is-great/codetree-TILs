ebcbac64da1e5f40511e2361993f3e2bdc13a9bb
simple-is-great <103080930+simple-is-great@users.noreply.github.com>
2024-07-19 17:21:56 +0900

    Title: 나누고 빼면서 합하기, Time: 119ms, Memory: 73MB, Status: Passed - CodeTree

--- 240719/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 240719/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -2,22 +2,23 @@ n, m = map(int, input().split())
 A = list(map(int, input().split()))
 sum_ = 0
 
-def modifyIndex(): # 수열이라 인덱스가 1부터 시작함에 주의
-    # 4 -> 2 -> 1
-    # 지금은 2 -> 1 순으로 실행
-    global m, A, sum_
-    if m == 1:
+def modifyIndex(M): 
+    # 수열이라 인덱스가 1부터 시작함에 주의
+    # 4 -> 2 -> 1, 지금은 2 -> 1 순으로 실행
+    global A, sum_
+
+    if M == 1:
         return
 
-    if m % 2 == 1:
-        m -= 1
-        sum_ += A[m - 1]
+    if M % 2 == 1:
+        M -= 1
+        sum_ += A[M - 1]
     else:
-        m //= 2
-        sum_ += A[m - 1]
+        M //= 2
+        sum_ += A[M - 1]
     
-    modifyIndex()
+    modifyIndex(M)
 
-sum_ += A[m-1]
-modifyIndex()
+sum_ += A[m - 1]
+modifyIndex(m)
 print(sum_)
